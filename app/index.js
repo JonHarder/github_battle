a3c89d8f99883f40a4e60201c523f23f6776581f
Jon Harder <jonharder6@gmail.com>
2017-07-19 15:45:02 -0500

    popular

--- app/index.js
+++ app/index.js
@@ -1,54 +1,11 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var PropTypes = require('prop-types');
+// var PropTypes = require('prop-types');
 require('./index.css');
+var App = require('./components/App');
 
-
-class Users extends React.Component {
-    render() {
-        return (
-            <div className="center">
-                <h1>{this.props.name}</h1>
-                <ul>
-                {this.props.list.map(p => <li key={p.name}>{p.name}</li>)}
-                </ul>
-            </div>
-        );
-    }
-}
-Users.propTypes = {
-    list: PropTypes.array.isRequired
-};
-
-
-class App extends React.Component {
-    render() {
-        let friends = this.props.people.filter(p => p.friend);
-        let nonFriends = this.props.people.filter(p => !p.friend);
-        return (
-            <div>
-                <Users name="Friends" list={friends}/>
-                <hr/>
-                <Users name="Non Friends" list={nonFriends}/>
-            </div>
-        );
-    }
-}
-App.propTypes = {
-    people: PropTypes.arrayOf(PropTypes.shape({
-        name: PropTypes.string.isRequired,
-        friend: PropTypes.bool.isRequired,
-    })),
-};
-
-var people = [
-    {name: "Rebecca", friend: true},
-    {name: "Steve", friend: false},
-    {name: "Sam", friend: true},
-    {name: "Bob", friend: false}
-];
 
 ReactDOM.render(
-    <App people={people}/>,
+    <App />,
     document.getElementById('app')
 );
